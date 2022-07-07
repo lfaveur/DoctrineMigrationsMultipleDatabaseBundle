adc51723500d4eafd15e5994810c0facce0cc253
lionel <lionel.faveur@agencedpc.fr>
2022-07-07 17:15:17 +0200

    imp

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace AvaiBookSports\Bundle\MigrationsMutlipleDatabase\DependencyInjection;
+namespace LFaveur\Bundle\MigrationsMutlipleDatabase\DependencyInjection;
 
 use Doctrine\Bundle\MigrationsBundle\DependencyInjection\Configuration as DoctrineMigrationsConfiguration;
 use ReflectionClass;
